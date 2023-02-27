470aff3b57d018852920d63111427ff974f7a7d5
RivAndrew <andrew_rivera1000@hotmail.com>
2023-02-26 21:25:30 -0700

    Se copió el contenido de las clases; 'Lista', 'Nodo' y 'Program', para modificarse y que funcionen de manera circular.

--- ListasDoblementeLigadasCirculares/Lista.cs
+++ ListasDoblementeLigadasCirculares/Lista.cs
@@ -8,5 +8,137 @@ namespace ListasDoblementeLigadasCirculares
 {
     internal class Lista
     {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+        public bool ValidaVacio()
+        {
+            if (nodoInicial.Siguiente == null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void VaciarLista()
+        {
+            nodoInicial.Siguiente = null;
+        }
+        public string RecorrerLista()
+        {
+            string valores = string.Empty;
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                valores += $"{nodoActual.Valor}\n";
+            }
+            return valores;
+        }
+        public void AgregarNodo(string valor)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+            Nodo nuevoNodo = new Nodo(valor);
+            nodoActual.Siguiente = nuevoNodo;
+            nuevoNodo.Anterior = nodoActual;
+        }
+        public void AgregarNodoInicio(string valor)
+        {
+            nodoActual = nodoInicial;
+            Nodo nuevoNodo = new Nodo(valor, nodoInicial, nodoActual.Siguiente);
+            if (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                nodoActual.Anterior = nuevoNodo;
+            }
+            nodoActual = nodoInicial;
+            nodoActual.Siguiente = nuevoNodo;
+        }
+        public Nodo Buscar(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Siguiente != null)
+                {
+                    nodoBusqueda = nodoBusqueda.Siguiente;
+                    if (nodoBusqueda.Valor == valor)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+        public Nodo BuscarPorIndice(int indice)
+        {
+            int Indice = -1;
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Siguiente != null)
+                {
+                    nodoBusqueda = nodoBusqueda.Siguiente;
+                    Indice++;
+                    if (Indice == indice)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+        public Nodo BuscarAnterior(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Siguiente != null
+                            && nodoBusqueda.Valor != valor)
+                {
+                    nodoBusqueda = nodoBusqueda.Siguiente;
+                    if (nodoBusqueda.Valor == valor)
+                    {
+                        return nodoBusqueda.Anterior;
+                    }
+                }
+            }
+            return null;
+        }
+        public void BorrarNodo(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                nodoActual = Buscar(valor);
+                if (nodoActual != null)
+                {
+                    nodoActual.Anterior.Siguiente = nodoActual.Siguiente;
+                    nodoActual.Siguiente.Anterior = nodoActual.Anterior;
+                    nodoActual.Siguiente = null;
+                    nodoActual.Anterior = null;
+                }
+            }
+        }
+        public string RecorrerListaInversa()
+        {
+            string valores = string.Empty;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+            while (nodoActual.Anterior != null && nodoActual.Anterior != nodoInicial)
+            {
+                valores += $"{nodoActual.Valor}\n";
+                nodoActual = nodoActual.Anterior;
+            }
+            valores += $"{nodoActual.Valor}\n";
+            return valores;
+        }
     }
 }
--- ListasDoblementeLigadasCirculares/Nodo.cs
+++ ListasDoblementeLigadasCirculares/Nodo.cs
@@ -8,5 +8,14 @@ namespace ListasDoblementeLigadasCirculares
 {
     internal class Nodo
     {
+        public string Valor { get; set; }
+        public Nodo Anterior { get; set; }
+        public Nodo Siguiente { get; set; }
+        public Nodo(string valor = "Inicio", Nodo anterior = null, Nodo siguente = null)
+        {
+            Valor = valor;
+            Anterior = anterior;
+            Siguiente = siguente;
+        }
     }
 }
